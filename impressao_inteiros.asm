4b3dc159226845a36ebbf213ec51f53305d80c16
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2022-11-30 16:24:16 -0300

    Teste

---
+++ impressao_inteiros.asm
@@ -0,0 +1,6 @@
+.data
+       idade: .word 19        #valor inteiro na memoria RAM
+.text
+      li $v0, 1              #imprimir inteiro
+      lw $a0, idade         #se usa para o tipo word
+      syscall              #faça
